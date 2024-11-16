b7ec110651ab44b83c6562c6ff2e3eb57c6299fa
Sanja Paskova <sanja.paskova@it-labs.com>
2024-11-16 22:23:16 +0100

    Add winston logger and implement logging in one of the tests

--- test/specs/app.drag.and.drop.test.js
+++ test/specs/app.drag.and.drop.test.js
@@ -1,11 +1,14 @@
-const dragAndDrop = require('../pageobjects/drag.screen.page.js');
+import dragAndDrop from '../pageobjects/drag.screen.page.js';
+import logger from '../../logger.js';
 
 describe('Drag and Drop tests', () => {
     it('should be able to drag and drop and complete the picture', async () => {
+        logger.info('Starting drag and drop test');
         await dragAndDrop.openDragAndDrop();
         await dragAndDrop.performDragAndDrop(await $('~drag-l1'), await $('~drop-l1'));
         await dragAndDrop.performDragAndDrop(await $('~drag-l2'), await $('~drop-l2'));
         await dragAndDrop.assertDragElementDoesNotExist();
+        logger.info('Completed drag and drop test');
     });
 
 });
