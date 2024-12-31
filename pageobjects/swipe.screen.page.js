37daeae1c3e6539ef07ad4e3ace9373efacf680a
ritech <ritech@ritechs-MacBook-Pro.local>
2024-12-31 11:59:01 +0100

    Fix locators in swipe.screen.page to be able to use them on iOS

--- pageobjects/swipe.screen.page.js
+++ pageobjects/swipe.screen.page.js
@@ -2,8 +2,15 @@ class SwipeScreenPage {
     get homeButton() { return $('~Home'); }
     get swipeButton() { return $('~Swipe'); }
     get carousel() { return $('~Carousel'); }
-    get carouselItem0() { return $('//android.view.ViewGroup[@resource-id="__CAROUSEL_ITEM_0_READY__"]'); }
-    get carouselItem1() { return $('//android.view.ViewGroup[@resource-id="__CAROUSEL_ITEM_1_READY__"]'); }
+
+    get carouselItem0() { return $(this.locatorStrategy("__CAROUSEL_ITEM_0_READY__"));}
+    get carouselItem1() { return $(this.locatorStrategy("__CAROUSEL_ITEM_1_READY__"));}
+
+    locatorStrategy(selector) {
+        return driver.isIOS
+            ? `~${selector}`
+            : `//*[@resource-id="${selector}"]`;
+    }
 
     async openSwipeScreen() {
         await this.homeButton.waitForDisplayed();
@@ -35,17 +42,17 @@ class SwipeScreenPage {
     async assertCarouselItemPosition(carouselItem) {
         const elem = await carouselItem;
         const cardRectangles = await driver.getElementRect(elem.elementId);
-        expect(cardRectangles.x === 0).toBeTruthy();
+        expect(cardRectangles.x === 0).toBeTruthy('Expected carousel item to be at position x = 0, but it is not.');
     }
 
     async assertCarouselItemDoesNotExist(carouselItem) {
         const doesExist = await carouselItem.isExisting();
-        expect(doesExist).toBeFalsy();
+        expect(doesExist).toBeFalsy('Expected carousel item not to exist, but it does.');
     }
 
     async assertCarouselItemIsDisplayed(carouselItem) {
         const cardRectangles = await driver.getElementRect((await carouselItem).elementId);
-        expect(cardRectangles.x === 0).toBeTruthy();
+        expect(cardRectangles.x === 0).toBeTruthy('Expected carousel item to be displayed, but it is not.');
     }
 }
 
